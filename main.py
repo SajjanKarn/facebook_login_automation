929ab4c392e8d9ecce2be14c582cdbfb30531cc7
ManishKarn56 <69583236+ManishKarn56@users.noreply.github.com>
2020-10-02 10:47:19 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from selenium import webdriver
 import creditentials
 
-# driver get
+url = "https://facebook.com"
+
 driver = webdriver.Firefox()
 
-driver.get("https://facebook.com")
+driver.get(url)
 
-
-# Navigating TO The elements
 email_input = driver.find_element_by_id("email")
 password = driver.find_element_by_id("pass")
 login_btn = driver.find_element_by_xpath("//*[@id=\"u_0_b\"]")
 
-# The Real Movies Begins....
 email_input.send_keys(creditentials.username)
 password.send_keys(creditentials.password)
 login_btn.click()
